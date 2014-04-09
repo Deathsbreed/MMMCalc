30d9e1189c902793b9f7bd2aa39e08d400c6d97d
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-04-09 17:22:39 -0500

    Added GNU GPLv3 copyright requirements to program.

--- src/MMMCalc.java
+++ src/MMMCalc.java
@@ -20,15 +20,39 @@ public class MMMCalc {
 	private static float varience = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to MMMCalc v0.2, a simple tool for basic statistics calculations.\n" +
-			"This software is licensed under the GNU GPLv3 license and comes WITHOUT WARRANTY.\n");
+		System.out.println("MMMCalc v0.2, Copyright (C) 2014 Nicolás A. Ortega\n" +
+					"This program comes with ABSOLUTELY NO WARRANTY; for details use '-w'\n" +
+					"This is free software, and you are welcome to redistribute it\n" +
+					"under certain conditions; use '-c' for details.\n");
 		if(args.length > 0) {
 			if(args[0].equals("-h")) {
 				System.out.println("Usage:\n" +
 					"  MMMCalc [options] [variables]\n\n" +
 					"Options:\n" +
-					"  -h -- Show this help information.\n" +
-					"  -v | -V -- Be verbose (show the work)\n");
+					"  -v | -V -- Be verbose (show the work).\n" +
+					"  -h -- Show this help information.\n");
+			} else if(args[0].equals("-w")) {
+				System.out.println("THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY\n" +
+					"APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT\n" +
+					"HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM 'AS IS' WITHOUT WARRANTY\n" +
+					"OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,\n" +
+					"THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n" +
+					"PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM\n" +
+					"IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF\n" +
+					"ALL NECESSARY SERVICING, REPAIR OR CORRECTION.\n");
+			} else if(args[0].equals("-c")) {
+				System.out.println("MMMCalc, a very basic statistics calculator.\n" +
+					"Copyright (C) 2014 Nicolás A. Ortega\n\n" +
+					"This program is free software: you can redistribute it and/or modify\n" +
+					"it under the terms of the GNU General Public License as published by\n" +
+					"the Free Software Foundation, either version 3 of the License, or\n" +
+					"(at your option) any later version.\n\n" +
+					"This program is distributed in the hope that it will be useful,\n" +
+					"but WITHOUT ANY WARRANTY; without even the implied warranty of\n" +
+					"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+					"GNU General Public License for more details.\n\n" +
+					"You should have received a copy of the GNU General Public License\n" +
+					"along with this program.  If not, see <www.gnu.org/licenses/>.");
 			} else if(args[0].equals("-v") || args[0].equals("-V")) {
 				verbose = true;
 				numArray = new float[args.length - 1];
