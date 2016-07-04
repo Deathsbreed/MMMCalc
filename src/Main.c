156f8de5c04df9d0bb4a405049a59d8436bd498e
Nicolás A. Ortega <nicolas.ortega.froysa@gmail.com>
2016-07-04 16:17:16 +0200

    Strict compiler with reldeb default build.

--- src/Main.c
+++ src/Main.c
@@ -5,7 +5,7 @@
 char version[16] = "v0.5";
 int verbose = 0;
 
-int main(int argc, char **argv) {
+int main(/*int argc, char **argv*/) {
     printf("MMMCalc %s, Copyright (c) 2016 Nicolás A. Ortega.\n", version);
     printf("This program comes with ABSOLUTELY NO WARRANTY.\n");
     printf("This program is free software and you are welcome to redistribute\n");
